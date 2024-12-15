fc7958d3303cc1514fe87478dce242771647f5a9
phs-informatica <paulohsantos2008@hotmail.com>
2024-12-14 22:21:35 -0300

    correção pagina imagens

--- src/componentes/Banner/banner.js
+++ src/componentes/Banner/banner.js
@@ -4,7 +4,7 @@ export const Banner = () => {
     // JSX
     return (
         <header className="banner">
-            <img src="/imagens/banner.png" alt="O banner principal da página do Organo"/>
+            <img src="/Imagens/banner.png" alt="O banner principal da página do Organo"/>
         </header>
     )
 }
